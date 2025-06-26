e860caee3c7f452aa9ff0f960ccbc7dba757720f
Nicolas Martin <nicolas.martin@arobas-music.com>
2025-06-26 15:12:13 +0200

    test functor

--- main.cpp
+++ main.cpp
@@ -3,13 +3,49 @@
 #include <gtest/gtest.h>
 
 
-TEST(SampleTest, BasicAssertions) {
-    // Expect equality.
-    EXPECT_EQ(1, 1);
-    // Expect inequality.
-    EXPECT_NE(1, 2);
-    // Expect true.
-    EXPECT_TRUE(true);
-    // Expect false.
-    EXPECT_FALSE(false);
+TEST(PackagedTask, ObjectFunctor) {
+   
+    struct Functor {
+        int *_instance_count{nullptr};
+        Functor(int* instance_count) : _instance_count(instance_count) {
+            ++*_instance_count;
+        }
+        ~Functor() {
+            if(_instance_count) {
+                --*_instance_count;
+            }
+        }
+        Functor(const Functor&) = delete;
+        Functor& operator=(const Functor&) = delete;
+        Functor(Functor&& o) noexcept : _instance_count(o._instance_count) {
+            o._instance_count = nullptr;
+        }
+        Functor& operator=(Functor&& o) {
+            if (this != &o) {
+                
+                _instance_count = o._instance_count;
+                o._instance_count = nullptr;
+            }
+            return *this;
+        }
+        void operator()() const {
+            // This is a simple functor that does nothing.
+        }
+    };
+    int instance_count = 0;
+
+    {
+        std::future<void> result;
+        {
+            std::packaged_task<void()> task(Functor{&instance_count});
+            EXPECT_EQ(instance_count, 1);
+            result = task.get_future();
+            task();
+        }
+        EXPECT_EQ(instance_count, 0);
+
+        result.get();
+        EXPECT_EQ(instance_count, 0);
+    }
+    EXPECT_EQ(instance_count, 0);
 }
